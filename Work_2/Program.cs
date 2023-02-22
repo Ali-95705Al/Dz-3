bdada54c60692da6e2d7d54c7bbc57659019d1ef
Ali-95705 <115585105+Ali-95705@users.noreply.github.com>
2023-02-22 22:28:35 +0300

    Work_2

---
+++ Work_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+int InputInt(string message)                                
+{
+    System.Console.Write($"{message}> ");                 
+    int value;                                            
+    if (int.TryParse(Console.ReadLine(), out value))     
+    {                                                     
+        return value;                                     
+    }                                                     
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(1);                                 
+    return 0;                                             
+}
+
+int number = InputInt("Введите число N, до которого выдадим квадраты: "); 
+
+for (int i = 1; i <= number; i++) 
+{  
+    int square = i * i;              
+    System.Console.Write($"{square} ");  
+}
